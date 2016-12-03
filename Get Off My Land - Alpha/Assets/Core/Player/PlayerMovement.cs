d5455dacabca761504404916b0196fac17691dd2
FireBolt-Studios <jamiewhite121988@gmail.com>
2016-12-03 02:03:28 +0000

    Uploaded project folder

---
+++ Get Off My Land - Alpha/Assets/Core/Player/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+	public Camera mainCamera;
+	private Vector3 mousePosition;
+
+	public Transform character;
+	public Transform cursorRef;
+
+	void LookAtCursor ()
+	{
+		cursorRef.position = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+		cursorRef.position = new Vector3(cursorRef.position.x,1,cursorRef.position.z);
+
+		character.LookAt(cursorRef);
+		character.localEulerAngles = new Vector3(90,character.localEulerAngles.y-90,0);
+
+	}
+
+	void Update ()
+	{
+		LookAtCursor();
+	}
+}
